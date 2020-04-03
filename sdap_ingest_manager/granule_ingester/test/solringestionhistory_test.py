0f86a58fe5c82a4c58e767033f74d5c97d30ac93
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-04-03 12:18:40 -0700

    deactivate tests with proper unittest decorator

--- sdap_ingest_manager/granule_ingester/test/solringestionhistory_test.py
+++ sdap_ingest_manager/granule_ingester/test/solringestionhistory_test.py
@@ -6,25 +6,27 @@ SOLR_URL = "http://localhost:8984/solr"
 
 
 class SolrIngestionHistoryTestCase(unittest.TestCase):
-    # def test_get(self):
-    #     ingestion_history = SolrIngestionHistory(SOLR_URL)
-    #
-    #     ingestion_history.push("zobi_la_mouche", "blue", "12weeukrhbwerqu7wier")
-    #
-    #     result = ingestion_history.get("zobi_la_mouche", "blue")
-    #
-    #     self.assertEqual(result.docs[0]['dataset_s'], "zobi_la_mouche")
-    #     self.assertEqual(result.docs[0]['granule_s'], "blue")
-    #     self.assertEqual(result.docs[0]['granule_md5sum_s'], "12weeukrhbwerqu7wier")
-    #
-    # def test_get_md5sum(self):
-    #     ingestion_history = SolrIngestionHistory(SOLR_URL)
-    #
-    #     ingestion_history.push("zobi_la_mouche", "blue", "12weeukrhbwerqu7wier")
-    #
-    #     result = ingestion_history.get_md5sum("zobi_la_mouche", "blue")
-    #
-    #     self.assertEqual(result, "12weeukrhbwerqu7wier")
+    @unittest.skip("does not work without a solr server for test")
+    def test_get(self):
+        ingestion_history = SolrIngestionHistory(SOLR_URL)
+
+        ingestion_history.push("zobi_la_mouche", "blue", "12weeukrhbwerqu7wier")
+
+        result = ingestion_history.get("zobi_la_mouche", "blue")
+
+        self.assertEqual(result.docs[0]['dataset_s'], "zobi_la_mouche")
+        self.assertEqual(result.docs[0]['granule_s'], "blue")
+        self.assertEqual(result.docs[0]['granule_md5sum_s'], "12weeukrhbwerqu7wier")
+
+    @unittest.skip("does not work without a solr server for test")
+    def test_get_md5sum(self):
+        ingestion_history = SolrIngestionHistory(SOLR_URL)
+
+        ingestion_history.push("zobi_la_mouche", "blue", "12weeukrhbwerqu7wier")
+
+        result = ingestion_history.get_md5sum("zobi_la_mouche", "blue")
+
+        self.assertEqual(result, "12weeukrhbwerqu7wier")
 
 
 if __name__ == '__main__':
