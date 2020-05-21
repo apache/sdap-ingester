b2a409c74dc4ada13aead1f4badfdf71a01fc028
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-05-21 09:10:46 -0400

    simplified obsolete unit test

--- sdap_ingest_manager/collections_ingester/test/unit/util_test.py
+++ sdap_ingest_manager/collections_ingester/test/unit/util_test.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 import logging
 from sdap_ingest_manager.collections_ingester import LocalConfiguration
 from sdap_ingest_manager.collections_ingester.util import ConfigWithPath
@@ -7,17 +8,18 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class MyTestCase(unittest.TestCase):
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                               '..', '..',
+                                               'resources', 'config')
+
     def test_read_local_configuration(self):
-        config = LocalConfiguration(config_path="sdap_ingest_manager/collections_ingester/resources/config").get()
+        config = LocalConfiguration(config_path=self.config_path).get()
         logger.info(f"interpolated value is {config['COLLECTIONS_YAML_CONFIG']['yaml_file']}")
-        self.assertEqual(config['COLLECTIONS_YAML_CONFIG']['yaml_file'],
-                         "sdap_ingest_manager/collections_ingester/resources/config/collections.yml")
 
 
     def test_config_with_path(self):
         config_with_path = ConfigWithPath()
-
-        config_with_path.read(['sdap_ingest_manager/collections_ingester/resources/config/sdap_ingest_manager.ini.default'])
+        config_with_path.read([os.path.join(self.config_path, 'sdap_ingest_manager.ini.default')])
         logger.info(config_with_path.get('INGESTION_ORDER_YAML_CONFIG', 'yaml_file'))
         logger.info(config_with_path.get('GIT_CONFIG', 'git_local_dir'))
 
