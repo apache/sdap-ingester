17bd28cb285f65681d9703271fe73e1d86be97c1
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-06-08 19:48:44 -0700

    apply change requests by pull request #3 review

--- config_operator/config_operator/config_source/LocalDirConfig.py
+++ config_operator/config_operator/config_source/LocalDirConfig.py
@@ -2,6 +2,7 @@ import os
 import time
 import logging
 import yaml
+from typing import Callable
 
 
 from config_operator.config_source.exceptions import UnreadableFileException
@@ -14,10 +15,12 @@ LISTEN_FOR_UPDATE_INTERVAL_SECONDS = 1
 
 class LocalDirConfig:
 
-    def __init__(self, local_dir):
+    def __init__(self, local_dir: str,
+                 update_every_seconds: int = LISTEN_FOR_UPDATE_INTERVAL_SECONDS):
         logger.info(f'create config on local dir {local_dir}')
         self._local_dir = local_dir
         self._latest_update = self._get_latest_update()
+        self._update_every_seconds=update_every_seconds
         
     def get_files(self):
         files = []
@@ -29,14 +32,14 @@ class LocalDirConfig:
 
         return files
 
-    def _test_read_yaml(self, file_name):
+    def _test_read_yaml(self, file_name: str):
         """ check yaml syntax raise yaml.parser.ParserError is it doesn't"""
         with open(os.path.join(self._local_dir, file_name), 'r') as f:
             docs = yaml.load_all(f, Loader=yaml.FullLoader)
             for doc in docs:
                 pass
 
-    def get_file_content(self, file_name):
+    def get_file_content(self, file_name: str):
         logger.info(f'read configuration file {file_name}')
         try:
             self._test_read_yaml(file_name)
@@ -55,9 +58,12 @@ class LocalDirConfig:
         else:
             return None
 
-    def when_updated(self, callback):
+    def when_updated(self, callback: Callable[[], None]):
+        """
+          call function callback when the local directory is updated.
+        """
         while True:
-            time.sleep(LISTEN_FOR_UPDATE_INTERVAL_SECONDS)
+            time.sleep(self._update_every_seconds)
             latest_update = self._get_latest_update()
             if latest_update is None or (latest_update > self._latest_update):
                 logger.info("local config dir has been updated")
@@ -65,4 +71,6 @@ class LocalDirConfig:
                 self._latest_update = latest_update
             else:
                 logger.debug("local config dir has not been updated")
+
+        return None
 
