d52996f41e3e475735d741143eb0d87624e74312
Eamon Ford <eamonford@gmail.com>
2020-08-03 12:24:20 -0700

    add setup.py to common

---
+++ common/setup.py
@@ -0,0 +1,21 @@
+import re
+
+import setuptools
+
+PACKAGE_NAME = "sdap_ingester_common"
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    author="Apache - SDAP",
+    author_email="dev@sdap.apache.org",
+    description="a helper to ingest data in sdap",
+    url="https://github.com/apache/incubator-sdap-ingester",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires='>=3.7',
+    include_package_data=True
+)
