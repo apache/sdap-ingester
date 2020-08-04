06fd27ec0822a548441d52e016889ea269205d95
Eamon Ford <eamonford@gmail.com>
2020-08-04 14:03:42 -0700

    Revert

--- granule_ingester/granule_ingester/main.py
+++ granule_ingester/granule_ingester/main.py
@@ -16,14 +16,13 @@
 import argparse
 import asyncio
 import logging
-import sys
 from functools import partial
 from typing import List
 
 from granule_ingester.consumer import Consumer
-from granule_ingester.exceptions import FailedHealthCheckError, LostConnectionError
 from granule_ingester.healthcheck import HealthCheck
-from granule_ingester.writers import CassandraStore, SolrStore
+from granule_ingester.writers import CassandraStore
+from granule_ingester.writers import SolrStore
 
 
 def cassandra_factory(contact_points, port, username, password):
@@ -32,8 +31,8 @@ def cassandra_factory(contact_points, port, username, password):
     return store
 
 
-def solr_factory(solr_host_and_port, zk_host_and_port):
-    store = SolrStore(zk_url=zk_host_and_port) if zk_host_and_port else SolrStore(solr_url=solr_host_and_port)
+def solr_factory(solr_host_and_port):
+    store = SolrStore(solr_host_and_port)
     store.connect()
     return store
 
@@ -45,7 +44,7 @@ async def run_health_checks(dependencies: List[HealthCheck]):
     return True
 
 
-async def main(loop):
+async def main():
     parser = argparse.ArgumentParser(description='Listen to RabbitMQ for granule ingestion instructions, and process '
                                                  'and ingest a granule for each message that comes through.')
     parser.add_argument('--rabbitmq_host',
@@ -85,8 +84,6 @@ async def main(loop):
                         default='http://localhost:8983',
                         metavar='HOST:PORT',
                         help='Solr host and port. (Default: http://localhost:8983)')
-    parser.add_argument('--zk_host_and_port',
-                        metavar="HOST:PORT")
     parser.add_argument('-v',
                         '--verbose',
                         action='store_true',
@@ -105,12 +102,11 @@ async def main(loop):
     config_values_str = "\n".join(["{} = {}".format(arg, getattr(args, arg)) for arg in vars(args)])
     logger.info("Using configuration values:\n{}".format(config_values_str))
 
-    cassandra_contact_points = args.cassandra_contact_points
-    cassandra_port = args.cassandra_port
     cassandra_username = args.cassandra_username
     cassandra_password = args.cassandra_password
+    cassandra_contact_points = args.cassandra_contact_points
+    cassandra_port = args.cassandra_port
     solr_host_and_port = args.solr_host_and_port
-    zk_host_and_port = args.zk_host_and_port
 
     consumer = Consumer(rabbitmq_host=args.rabbitmq_host,
                         rabbitmq_username=args.rabbitmq_username,
@@ -121,28 +117,19 @@ async def main(loop):
                                                    cassandra_port,
                                                    cassandra_username,
                                                    cassandra_password),
-                        metadata_store_factory=partial(solr_factory, solr_host_and_port, zk_host_and_port))
-    try:
-        solr_store = SolrStore(zk_url=zk_host_and_port) if zk_host_and_port else SolrStore(solr_url=solr_host_and_port)
-        await run_health_checks([CassandraStore(cassandra_contact_points,
-                                                cassandra_port,
-                                                cassandra_username,
-                                                cassandra_password),
-                                 solr_store,
-                                 consumer])
+                        metadata_store_factory=partial(solr_factory, solr_host_and_port))
+    if await run_health_checks([CassandraStore(cassandra_contact_points,
+                                               cassandra_port,
+                                               cassandra_username,
+                                               cassandra_password),
+                                SolrStore(solr_host_and_port),
+                                consumer]):
         async with consumer:
             logger.info("All external dependencies have passed the health checks. Now listening to message queue.")
             await consumer.start_consuming()
-    except FailedHealthCheckError as e:
-        logger.error(f"Quitting because not all dependencies passed the health checks: {e}")
-    except LostConnectionError as e:
-        logger.error(f"{e} Any messages that were being processed have been re-queued. Quitting.")
-    except Exception as e:
-        logger.exception(f"Shutting down because of an unrecoverable error:\n{e}")
-    finally:
-        sys.exit(1)
+    else:
+        logger.error("Quitting because not all dependencies passed the health checks.")
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
+    asyncio.run(main())
