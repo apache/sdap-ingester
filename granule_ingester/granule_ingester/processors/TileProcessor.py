4c758c07682a3cc34bb35b262e6b26ead470c81d
echyam <echyam@gmail.com>
2020-11-02 13:39:45 -0800

    add commented skeleton code for tileprocessor abstract method process

--- granule_ingester/granule_ingester/processors/TileProcessor.py
+++ granule_ingester/granule_ingester/processors/TileProcessor.py
@@ -14,10 +14,29 @@
 # limitations under the License.
 
 from abc import ABC, abstractmethod
+from nexusproto.serialization import from_shaped_array, to_shaped_array
+from nexusproto.DataTile_pb2 import NexusTile
+from granule_ingester.processors.TileProcessor import TileProcessor
 
 
 # TODO: make this an informal interface, not an abstract class
 class TileProcessor(ABC):
     @abstractmethod
-    def process(self, tile, *args, **kwargs):
+    def process(self, tile: NexusTile, *args, **kwargs):
+        ## accessing the data
+        # the_tile_type = tile.tile.WhichOneof("tile_type")
+        # the_tile_data = getattr(tile.tile, the_tile_type)
+
+        ## get netCDF as xarray.Dataset object        
+        # ds = kwargs['dataset']
+
+        ## example transformation:
+        # var_data = from_shaped_array(the_tile_data.variable_data) - 273.15
+
+        ## save transformed data back into tile
+        # the_tile_data.variable_data.CopyFrom(to_shaped_array(var_data))
+
+        ## return transformed tile
+        # return tile
+
         pass
