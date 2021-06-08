d4c978c14155cfed16e872d89f86f5964a2ea24b
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-06-08 16:50:15 -0700

    fix: update K2C logic for vectored input

--- granule_ingester/granule_ingester/processors/kelvintocelsius.py
+++ granule_ingester/granule_ingester/processors/kelvintocelsius.py
@@ -12,32 +12,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
 
 from nexusproto.serialization import from_shaped_array, to_shaped_array
 from nexusproto.DataTile_pb2 import NexusTile
 from granule_ingester.processors.TileProcessor import TileProcessor
 
+logger = logging.getLogger(__name__)
+
 
 class KelvinToCelsius(TileProcessor):
+    def __retrieve_var_units(self, variable_name, ds):
+        variable_unit = []
+        for each in variable_name:
+            if 'units' in ds.variables[each].attrs:
+                variable_unit.extend(ds.variables[each].attrs['units'])
+            elif 'Units' in ds.variables[each].attrs:
+                variable_unit.extend(ds.variables[each].attrs['Units'])
+            elif 'UNITS' in ds.variables[each].attrs:
+                variable_unit.extend(ds.variables[each].attrs['UNITS'])
+        return variable_unit
+
     def process(self, tile: NexusTile, *args, **kwargs):
+        logger.debug(f'KelvinToCelsius kwargs: {kwargs}')
         the_tile_type = tile.tile.WhichOneof("tile_type")
         the_tile_data = getattr(tile.tile, the_tile_type)
+        logger.debug(the_tile_data.variable_data)
         kelvins = ['kelvin', 'degk', 'deg_k', 'degreesk', 'degrees_k', 'degree_k', 'degreek']
 
         if 'dataset' in kwargs:
             ds = kwargs['dataset']
             variable_name = tile.summary.data_var_name
-            if 'units' in ds.variables[variable_name].attrs:
-                variable_unit = ds.variables[variable_name].attrs['units']
-            elif 'Units' in ds.variables[variable_name].attrs:
-                variable_unit = ds.variables[variable_name].attrs['Units']
-            elif 'UNITS' in ds.variables[variable_name].attrs:
-                variable_unit = ds.variables[variable_name].attrs['UNITS']
-            else:
+            logger.debug(f'K2C tile.summary.data_var_name: {variable_name}')
+            logger.debug(f'ds.variables: {ds.variables}')
+            logger.warning(f'forcing to return for now')
+            variable_unit = self.__retrieve_var_units(variable_name, ds)
+            if len(variable_unit) < 1:
                 return tile
-
-            if any([unit in variable_unit.lower() for unit in kelvins]):
+            variable_unit = [k.lower() for k in variable_unit]
+            if any([unit in variable_unit for unit in kelvins]):
                 var_data = from_shaped_array(the_tile_data.variable_data) - 273.15
                 the_tile_data.variable_data.CopyFrom(to_shaped_array(var_data))
-
         return tile
