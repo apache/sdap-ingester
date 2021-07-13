5a0ba29f4f794f2f49add8f56cda5d83bf0cb632
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-07-13 05:50:36 -0700

    feat: make incoming configmap backward compatible by allowing either variable or variables

--- collection_manager/collection_manager/entities/Collection.py
+++ collection_manager/collection_manager/entities/Collection.py
@@ -31,16 +31,39 @@ class Collection:
     date_to: Optional[datetime] = None
 
     @staticmethod
+    def __decode_dimension_names(dimension_names_dict):
+        """
+        - Validating both `variable` and `variables` are not part of the dictionary
+        - if it has `variable`, converting it to single element list
+        - if it has `variables`, keeping it as a list while renmaing the key to `variable`
+        """
+        if 'variable' in dimension_names_dict and 'variables' in dimension_names_dict:
+            raise RuntimeError('both variable and variables present in dimensionNames. Only one is allowed')
+        new_dimension_names = [(k, v) for k, v in dimension_names_dict.items() if k not in ['variable', 'variables']]
+        if 'variable' in dimension_names_dict:
+            if not isinstance(dimension_names_dict['variable'], str):
+                raise RuntimeError(f'variable in dimensionNames must be string type. value: {dimension_names_dict["variable"]}')
+            new_dimension_names.append(('variable', str([dimension_names_dict['variable']])))
+            return new_dimension_names
+        if 'variables' in dimension_names_dict:
+            if not isinstance(dimension_names_dict['variables'], list):
+                raise RuntimeError(f'variable in dimensionNames must be list type. value: {dimension_names_dict["variables"]}')
+            new_dimension_names.append(('variable', str(dimension_names_dict['variables'])))
+            return new_dimension_names
+
+    @staticmethod
     def from_dict(properties: dict):
+        """
+        Accepting either `variable` or `variables` from the configmap
+        """
         logger.debug(f'incoming properties dict: {properties}')
         try:
             date_to = datetime.fromisoformat(properties['to']) if 'to' in properties else None
             date_from = datetime.fromisoformat(properties['from']) if 'from' in properties else None
 
-            dimension_names = [(k, str(v) if isinstance(v, list) else v) for k, v in properties['dimensionNames'].items()]
             collection = Collection(dataset_id=properties['id'],
                                     projection=properties['projection'],
-                                    dimension_names=frozenset(dimension_names),
+                                    dimension_names=frozenset(Collection.__decode_dimension_names(properties['dimensionNames'])),
                                     slices=frozenset(properties['slices'].items()),
                                     path=properties['path'],
                                     historical_priority=properties['priority'],
