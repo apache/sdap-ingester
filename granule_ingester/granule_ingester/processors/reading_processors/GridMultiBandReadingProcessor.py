68175f1ba746c1bd54abe49215e14fd414717d15
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-07-26 13:42:47 -0700

    feat: keep grid & swath reading processor as single band + create 2 new processors for multiband

--- granule_ingester/granule_ingester/processors/reading_processors/GridMultiBandReadingProcessor.py
+++ granule_ingester/granule_ingester/processors/reading_processors/GridMultiBandReadingProcessor.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict
 
 import numpy as np
@@ -7,17 +8,32 @@ from nexusproto.serialization import to_shaped_array
 
 from granule_ingester.processors.reading_processors.TileReadingProcessor import TileReadingProcessor
 
+logger = logging.getLogger(__name__)
 
-class GridReadingProcessor(TileReadingProcessor):
+
+class GridMultiBandReadingProcessor(TileReadingProcessor):
     def __init__(self, variable, latitude, longitude, depth=None, time=None, **kwargs):
         super().__init__(variable, latitude, longitude, **kwargs)
-        if isinstance(variable, list) and len(variable) != 1:
-            raise RuntimeError(f'TimeSeriesReadingProcessor does not support multiple variable: {variable}')
         self.depth = depth
         self.time = time
 
     def _generate_tile(self, ds: xr.Dataset, dimensions_to_slices: Dict[str, slice], input_tile):
-        data_variable = self.variable[0] if isinstance(self.variable, list) else self.variable
+        """
+        Update 2021-05-28 : adding support for banded datasets
+            - self.variable can be a string or a list of strings
+            - if it is a string, keep the original workflow
+            - if it is a list, loop over the variable property array which will be the name of several datasets
+            - dimension 0 will be the defined list of datasets from parameters
+            - dimension 1 will be latitude
+            - dimension 2 will be longitude
+            - need to switch the dimensions
+            - dimension 0: latitude, dimension 1: longitude, dimension 2: defined list of datasets from parameters
+        Update 2021-07-09: temporarily cancelling dimension switches as it means lots of changes on query side.
+        :param ds: xarray.Dataset - netcdf4 object
+        :param dimensions_to_slices: Dict[str, slice] - slice dict with keys as the keys of the netcdf4 datasets
+        :param input_tile: nexusproto.NexusTile()
+        :return: input_tile - filled with the value
+        """
         new_tile = nexusproto.GridTile()
 
         lat_subset = ds[self.latitude][type(self)._slices_for_variable(ds[self.latitude], dimensions_to_slices)]
@@ -25,9 +41,22 @@ class GridReadingProcessor(TileReadingProcessor):
         lat_subset = np.ma.filled(np.squeeze(lat_subset), np.NaN)
         lon_subset = np.ma.filled(np.squeeze(lon_subset), np.NaN)
 
-        data_subset = ds[data_variable][type(self)._slices_for_variable(ds[data_variable],
-                                                                        dimensions_to_slices)]
-        data_subset = np.ma.filled(np.squeeze(data_subset), np.NaN)
+        if isinstance(self.variable, list):
+            logger.debug(f'reading as banded grid as self.variable is a list. self.variable: {self.variable}')
+            if len(self.variable) < 1:
+                raise ValueError(f'list of variable is empty. Need at least 1 variable')
+            data_subset = [ds[k][type(self)._slices_for_variable(ds[k], dimensions_to_slices)] for k in self.variable]
+            data_subset = np.ma.filled(np.squeeze(data_subset), np.NaN)
+            # Update: 2021-07-09: temporarily cancelling dimension switches
+            # if len(self.variable) > 1:
+            #     logger.debug(f'self.variable has more than 1 band. need to transpose')
+            #     transpose_arguments = [k for k in range(1, len(data_subset.shape))]
+            #     transpose_arguments.append(0)
+            #     data_subset = data_subset.transpose(*transpose_arguments)
+        else:
+            logger.debug(f'reading as normal grid as self.variable is not a list. Assuming it is a string. self.variable: {self.variable}')
+            data_subset = ds[self.variable][type(self)._slices_for_variable(ds[self.variable], dimensions_to_slices)]
+            data_subset = np.ma.filled(np.squeeze(data_subset), np.NaN)
 
         if self.depth:
             depth_dim, depth_slice = list(type(self)._slices_for_variable(ds[self.depth],
--- granule_ingester/tests/reading_processors/test_GridReadingProcessor.py
+++ granule_ingester/tests/reading_processors/test_GridReadingProcessor.py
@@ -18,11 +18,6 @@ from os import path
 
 import numpy as np
 import xarray as xr
-from granule_ingester.processors import ForceAscendingLatitude
-from granule_ingester.processors.EmptyTileFilter import EmptyTileFilter
-from granule_ingester.processors.Subtract180FromLongitude import Subtract180FromLongitude
-from granule_ingester.processors.TileSummarizingProcessor import TileSummarizingProcessor
-from granule_ingester.processors.kelvintocelsius import KelvinToCelsius
 from nexusproto import DataTile_pb2 as nexusproto
 from nexusproto.serialization import from_shaped_array
 
@@ -316,112 +311,6 @@ class TestReadInterpEccoData(unittest.TestCase):
 
 
 class TestReadHLSData(unittest.TestCase):
-    def test_101_preprocessed_data(self):
-        reading_processor = GridReadingProcessor([f'b{k}' for k in range(2, 8)], 'lat', 'long', time='time')
-        # granule_path = path.join(path.dirname(__file__), '../granules/s1_output_latlon_HLS_S30_T18TYN_2019070.nc')
-        granule_path = path.join(path.dirname(__file__), '../granules/s1_output_latlon_HLS_S30_T18TYN_2019363.nc')
-
-        input_tile = nexusproto.NexusTile()
-        input_tile.summary.granule = granule_path
-
-        dimensions_to_slices = {
-            'time': slice(0, 1),
-            'lat': slice(500, 550),
-            'long': slice(500, 550),
-        }
-
-        with xr.open_dataset(granule_path) as ds:
-            generated_tile = reading_processor._generate_tile(ds, dimensions_to_slices, input_tile)
-        empty_filter = EmptyTileFilter().process(generated_tile)
-        self.assertNotEqual(empty_filter, None, f'empty_filter is None')
-        subtract_180 = Subtract180FromLongitude().process(empty_filter)
-        self.assertNotEqual(subtract_180, None, f'subtract_180 is None')
-        force_asc = ForceAscendingLatitude().process(empty_filter)
-        self.assertNotEqual(force_asc, None, f'force_asc is None')
-        kelvin = KelvinToCelsius().process(force_asc)
-        self.assertNotEqual(kelvin, None, f'kelvin is None')
-        with xr.open_dataset(granule_path, decode_cf=True) as ds:
-            kelvin.summary.data_var_name.extend([f'b{k}' for k in range(2, 8)])
-            summary = TileSummarizingProcessor('test').process(kelvin, ds)
-            self.assertNotEqual(summary, None, f'summary is None')
-
-        tile_type = generated_tile.tile.WhichOneof("tile_type")
-        tile_data = getattr(generated_tile.tile, tile_type)
-        # latitudes = from_shaped_array(tile_data.latitude)
-        # longitudes = from_shaped_array(tile_data.longitude)
-        variable_data = from_shaped_array(tile_data.variable_data)
-        # print(variable_data)
-        self.assertEqual(granule_path, generated_tile.summary.granule, granule_path)
-        self.assertEqual(1577577600, generated_tile.tile.grid_tile.time)
-        self.assertEqual([6, 50, 50], generated_tile.tile.grid_tile.variable_data.shape)
-        self.assertEqual([50], generated_tile.tile.grid_tile.latitude.shape)
-        self.assertEqual([50], generated_tile.tile.grid_tile.longitude.shape)
-
-        return
-
-    def test_01(self):
-        reading_processor = GridReadingProcessor([f'B{k:02d}' for k in range(1, 12)], 'lat', 'lon', time='time')
-        granule_path = path.join(path.dirname(__file__), '../granules/HLS.S30.T11SPC.2020001.v1.4.hdf.nc')
-
-        input_tile = nexusproto.NexusTile()
-        input_tile.summary.granule = granule_path
-
-        dimensions_to_slices = {
-            'time': slice(0, 1),
-            'lat': slice(0, 30),
-            'lon': slice(0, 30),
-            # 'lat': slice(0, 200),
-            # 'lon': slice(0, 200),
-        }
-
-        with xr.open_dataset(granule_path) as ds:
-            generated_tile = reading_processor._generate_tile(ds, dimensions_to_slices, input_tile)
-        tile_type = generated_tile.tile.WhichOneof("tile_type")
-        tile_data = getattr(generated_tile.tile, tile_type)
-        # latitudes = from_shaped_array(tile_data.latitude)
-        # longitudes = from_shaped_array(tile_data.longitude)
-        # variable_data = from_shaped_array(tile_data.variable_data)
-        # print(generated_tile.tile.grid_tile.variable_data)
-        self.assertEqual(granule_path, generated_tile.summary.granule, granule_path)
-        self.assertEqual(1577836800, generated_tile.tile.grid_tile.time)
-        self.assertEqual([11, 30, 30], generated_tile.tile.grid_tile.variable_data.shape)
-        self.assertEqual([30], generated_tile.tile.grid_tile.latitude.shape)
-        self.assertEqual([30], generated_tile.tile.grid_tile.longitude.shape)
-
-        return
-
-    # def test_02(self):
-    #     reading_processor = GridReadingProcessor('B03', 'lat', 'lon', time='time')
-    #     granule_path = path.join(path.dirname(__file__), '../granules/HLS.S30.T11SPC.2020001.v1.4.hdf.nc')
-    #
-    #     input_tile = nexusproto.NexusTile()
-    #     input_tile.summary.granule = granule_path
-    #
-    #     dimensions_to_slices = {
-    #         'time': slice(0, 1),
-    #         'lat': slice(0, 30),
-    #         'lon': slice(0, 30)
-    #     }
-    #
-    #     with xr.open_dataset(granule_path) as ds:
-    #         generated_tile = reading_processor._generate_tile(ds, dimensions_to_slices, input_tile)
-    #     tile_type = generated_tile.tile.WhichOneof("tile_type")
-    #     tile_data = getattr(generated_tile.tile, tile_type)
-    #     latitudes = from_shaped_array(tile_data.latitude)
-    #     longitudes = from_shaped_array(tile_data.longitude)
-    #     variable_data = from_shaped_array(tile_data.variable_data)
-    #
-    #     self.assertEqual(granule_path, generated_tile.summary.granule, granule_path)
-    #     self.assertEqual(1577836800, generated_tile.tile.grid_tile.time)
-    #     self.assertEqual([30, 30], generated_tile.tile.grid_tile.variable_data.shape)
-    #     self.assertEqual([30], generated_tile.tile.grid_tile.latitude.shape)
-    #     self.assertEqual([30], generated_tile.tile.grid_tile.longitude.shape)
-    #
-    #     # print(latitudes)
-    #     # print(longitudes)
-    #     # print(variable_data)
-    #     return
-
     def test_03(self):
         reading_processor = GridReadingProcessor(['B03'], 'lat', 'lon', time='time')
         granule_path = path.join(path.dirname(__file__), '../granules/HLS.S30.T11SPC.2020001.v1.4.hdf.nc')
