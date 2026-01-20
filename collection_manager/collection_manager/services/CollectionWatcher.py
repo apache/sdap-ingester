f0adadecc3c3067d550f3c560f8824a2893a2d77
rileykk <rileykk@jpl.nasa.gov>
2026-01-20 15:36:43 -0800

    Add mismatch path to error log

--- collection_manager/collection_manager/services/CollectionWatcher.py
+++ collection_manager/collection_manager/services/CollectionWatcher.py
@@ -243,8 +243,8 @@ class _GranuleEventHandler(FileSystemEventHandler):
                     self._loop.create_task(self._callback(path, modified_time, collection))
                 else:
                     logger.error(f'Event for file {path} will be discarded as it is not owned by the collection it '
-                                 f'matched to: {collection.dataset_id}. This should not happen. Please report this '
-                                 f'with the relevant logs and collection configuration to dev@sdap.apache.org or '
-                                 f'https://issues.apache.org/jira/projects/SDAP/issues/')
+                                 f'matched to: {collection.dataset_id} ({collection.path}). This should not happen. '
+                                 f'Please report this with the relevant logs and collection configuration to '
+                                 f'dev@sdap.apache.org or https://issues.apache.org/jira/projects/SDAP/issues/')
             except IsADirectoryError:
                 return
